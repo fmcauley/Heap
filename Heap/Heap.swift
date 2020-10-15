d2ea67628f947948ece1148fd0ab04caa8195861
Frank McAuley <frank.mcauley@gmail.com>
2020-10-15 10:00:26 -0400

    Heap
    FM - Working from the outside in to wrap my head around how an abstract type like a Heap works. Starting with generating a function that finds the parent given an input index.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

--- Heap/Heap.swift
+++ Heap/Heap.swift
@@ -6,6 +6,27 @@
 //
 
 import Foundation
+
+/*
+ Notes on calculating values:
+ 1) Parent [i/2] can be computed by shifting i right one bit position
+ 2) Left 2i can be computed by shifting the binary representation of i left one bit position
+ 3) Right 2i + 1 can be computed by shifting the binary representation of i left one bit then
+ adding in a one as the low-order bit
+ */
+
 class Heap {
     
+    let baseArray : [Int]
+    
+    init(_ baseArray: [Int]) {
+        self.baseArray = baseArray
+    }
+    
+    // lets build out an interface for an abstract type: Heap
+    // A heap has a parent at [i/2]
+    func getParent(_ index: Int) -> Int {
+        let indexValue = index/2
+        return self.baseArray[indexValue]
+    }
 }
--- HeapTests/HeapTests.swift
+++ HeapTests/HeapTests.swift
@@ -19,8 +19,43 @@ class HeapTests: XCTestCase {
     }
 
     func testThatHeapIsNotNil() throws {
-        let heap = Heap()
+        let heap = Heap([0])
         XCTAssertNotNil(heap)
+    }
+    
+    func testThatAHeapTakesAnArrayAsAnInput() {
+        let input = [1]
+        let heap = Heap(input)
+        let output = heap.baseArray.count
+        let expected = 1
+        
+        XCTAssertEqual(output, expected)
+    }
+    
+    func testThatAHeapHasARootNode() throws {
+        let input = [42]
+        let heap = Heap(input)
+        let output = heap.getParent(0)
+        let expected = 42
+        
+        XCTAssertEqual(output, expected)
+    }
+    
+    func testThatAHeapWillReturnParentWithTwoElementArray() throws {
+        let input = [77,88]
+        let heap = Heap(input)
+        let output = heap.getParent(0)
+        let expected = 77
+        
+        XCTAssertEqual(output, expected)
+    }
+    
+    func testThatHeapCanFindAParentWithAnIndexThatIsPassedIn() throws {
+        let input = [99,101]
+        let heap = Heap(input)
+        let output = heap.getParent(1)
+        let expected = 99
+        XCTAssertEqual(output, expected)
     }
 
 }
